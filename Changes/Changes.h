74fed8ed4cb397ff1f41fbc4fe44abe97e9f1149
Chris Amanse <christopheramanse@gmail.com>
2016-01-21 09:25:48 +0800

    Initial Commit

---
+++ Changes/Changes.h
@@ -0,0 +1,19 @@
+//
+//  Changes.h
+//  Changes
+//
+//  Created by Chris Amanse on 1/21/16.
+//  Copyright © 2016 Joe Christopher Paul Amanse. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Changes.
+FOUNDATION_EXPORT double ChangesVersionNumber;
+
+//! Project version string for Changes.
+FOUNDATION_EXPORT const unsigned char ChangesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Changes/PublicHeader.h>
+
+
